37b7d4ddb12fc891309e2c57eefc8676401743a2
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-01-03 23:32:30 -0500

    Added form and fetch request to REST Countries API

--- Main/src/App.tsx
+++ Main/src/App.tsx
@@ -1,11 +1,33 @@
-import { useState } from 'react';
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+
+type Currency = {
+  name: string,
+  symbol: string,
+}
 
 function App() {
+  const [countryName, setCountryName] = useState("");
+  const [currency, setCurrency] = useState<Currency>({name: "", symbol: ""});
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    fetch(`https://restcountries.com/v3/name/poland`)
+    .then((response => response.json()))
+  }
 
   return (
     <div className="App">
-      
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="search-input">Country Name</label>
+        <input 
+          type="text" 
+          id="search-input" 
+          value={countryName} 
+          onChange={(e) => setCountryName(e.target.value)}
+        />
+        <button className="search-button">Search</button>
+      </form>
     </div>
   )
 }
